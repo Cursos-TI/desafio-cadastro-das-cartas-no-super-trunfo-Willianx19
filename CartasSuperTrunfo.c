64cb6e9e7efabc16c01b20b8651ed138d7f71a79
Willianx19 <williansoares2015@outlook.com.br>
2025-04-22 18:54:37 +0000

    Desafio nivel Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
 char Estado;
 char Codigo_da_Carta[5];
 char Nome_da_Cidade[30];
-int Populacao;
+unsigned long int Populacao;
 float Area;
 float Pib;
 int Numero_de_Pontos_Turisticos;
@@ -29,7 +29,7 @@ printf("Digite o Nome da Cidade: \n");
 scanf("%s",&Nome_da_Cidade);
 
 printf("Digite a População: \n");
-scanf("%d",&Populacao);
+scanf("%u",&Populacao);
 
 printf("Digite a Área: \n");
 scanf("%f",&Area);
@@ -51,7 +51,7 @@ Super_poder = Populacao + Area + Pib + Numero_de_Pontos_Turisticos + densidade_p
 printf("O Estado é: %c \n",Estado);
 printf("O Código da Cidade é: %s \n",Codigo_da_Carta);
 printf("O Nome da Cidade é: %s \n",Nome_da_Cidade );
-printf("A População é: %d \n",Populacao);
+printf("A População é: %u \n",Populacao);
 printf("A Área é: %f km² \n",Area);
 printf("O Pib é: %f bilhões de reais \n",Pib);
 printf("o Números de Pontos Turisticos são: %d \n",Numero_de_Pontos_Turisticos);
@@ -65,7 +65,7 @@ printf("o Super poder é: %f \n",Super_poder);
 char Estado02;
 char Codigo_do_Estado02[5];
 char Nome_da_cidade02[30];
-int Populacao02;
+unsigned long int Populacao02;
 float Area02;
 float Pib02;
 int Numero_turistico02;
@@ -86,7 +86,7 @@ printf("Digite o Nome da Cidade 02: \n");
 scanf("%s",&Nome_da_cidade02);
 
 printf("Digite a População 02: \n");
-scanf("%d",&Populacao02);
+scanf("%u",&Populacao02);
 
 printf("Digite a Área 02: \n");
 scanf("%f",&Area02);
@@ -108,7 +108,7 @@ super_poder2 = Populacao02 + Area02 + Pib02 + Numero_turistico02 + densidade_pop
 printf("O Estado da Carta 2 é: %c \n",Estado02);
 printf("o Codigo 02 é: %s \n",Codigo_do_Estado02);
 printf("O Nome da Cidade 02 é: %s \n",Nome_da_cidade02);
-printf("A Popolação 02 é: %d \n",Populacao02);
+printf("A Popolação 02 é: %u \n",Populacao02);
 printf("A Área 02 é: %f km²\n",Area02);
 printf("O Pib 02 é: %f bilhões de reais \n",Pib02);
 printf("Os Números de Pontos Turisticos 02 são: %d \n",Numero_turistico02);
@@ -129,12 +129,12 @@ resultado7 = Super_poder > super_poder2;
 
 // Printf das Comparações
 printf("Comparação das Cartas: \n");
-printf("População: carta 0! venceu (%f) \n",resultado);
-printf("Área: carta 01 venceu (%f) \n",resultado2);
+printf("População: carta 01 venceu (%d) \n",resultado);
+printf("Área: carta 01 venceu (%d) \n",resultado2);
 printf("Pib: carta 01 venceu (%d) \n",resultado3);
-printf("Pontos Turisticos: carta 01 venceu (%f) \n",resultado4);
-printf("Densidade populacional: carta 02 venceu (%f) \n",resultado5);
-printf("Pib per capita: carta 01 venceu (%f) \n",resultado6);
+printf("Pontos Turisticos: carta 01 venceu (%d) \n",resultado4);
+printf("Densidade populacional: carta 02 venceu (%d) \n",resultado5);
+printf("Pib per capita: carta 01 venceu (%d) \n",resultado6);
 printf("Super poder: Carta 01 venceu (%d) \n",resultado7);
 
 
